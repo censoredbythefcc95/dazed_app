ff5e3859d282908a3edf243af041bc23ab9367b8
Steven Young <sastudillo6@gmail.com>
2023-11-25 20:49:20 -0500

    added some more routes.

---
+++ backend/quotes.js
@@ -0,0 +1,64 @@
+const quotes = [
+    {
+        id: 1,
+        character: "Wooderson",
+        quote: "That's what I love about these high school girls, man.",
+        location: "Emporium Parking Lot",
+    },
+    {
+        id: 2,
+        character: "Mitch Kramer",
+        quote: "Cool? What do you mean?",
+        location: "Emporium Parking Lot",
+    },
+    {
+        id: 3,
+        character: "Pink",
+        quote: "Alright, alright, alright!",
+        location: "Various",
+    },
+    {
+        id: 4,
+        character: "Darla",
+        quote: "And you thought I was gonna be a bitch.",
+        location: "Emporium Party",
+    },
+    {
+        id: 5,
+        character: "Don Dawson",
+        quote: "You just gotta keep livin' man, L-I-V-I-N.",
+        location: "Emporium Parking Lot",
+    },
+    {
+        id: 6,
+        character: "Slater",
+        quote: "See, there's three of us and three of y'all.",
+        location: "Emporium Parking Lot",
+    },
+    {
+        id: 7,
+        character: "Mike Newhouse",
+        quote: "Check you later.",
+        location: "Various",
+    },
+    {
+        id: 8,
+        character: "O'Bannion",
+        quote: "Pick me up some shorts.",
+        location: "Various",
+    },
+    {
+        id: 9,
+        character: "Melvin Spivey",
+        quote: "No More Mr. Nice Guy.",
+        location: "Various",
+    },
+    {
+        id: 10,
+        character: "Sabrina Davis",
+        quote: "I've got an Aerosmith backstage pass.",
+        location: "Various",
+    },
+];
+
+module.exports = quotes;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const quotes = require('./quotes');
 
 app.get('/', (req, res) => {
     res.send('Hello - this is testing the backend server for the Dazed and Confused App.');
 });
 
-const PORT = 5000;
+app.get('/api/quotes', (req, res) => {
+    res.json(quotes);
+});
+
+app.get('/api/quotes/:id', (req, res) => {
+    const quoteId = parseInt(req.params.id);
+    const quote = quotes.find(q => q.id === quoteId);
+
+    if (!quote) {
+        res.status(404).json({ error: "Quote not found."});
+    } else {
+        res.json(quote);
+    }
+});
+
+const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is now running on http://localhost:${PORT}`);
